2ba28ec990d7f1f2e4484a0ad41dbc2a3b9344db
pedro <pedrohbustamante@icloud.com>
2024-09-23 20:25:00 -0300

    header add

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,8 +11,6 @@ export function Header() {
         <img src={HeaderLogo} alt="Logotipo da ONG" />
         <a href="">Sobre nós</a>
         <a href="">Adote</a>
-        <a href="">Brechó</a>
-        <a href="">Eventos</a>
         <a href="">Contribua</a>
         <a href="">Fale conosco</a>
       </Links>
