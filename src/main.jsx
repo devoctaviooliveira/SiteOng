a177cb1529f951029192c1017711d5ec2fb7c529
pedro <pedrohbustamante@icloud.com>
2024-09-23 20:03:48 -0300

    pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles/global.js'
 import { Homepage } from './pages/Homepage/index.jsx'
 import { StandardButton } from './components/StandardButton/index.jsx'
+import { FaleConosco } from './pages/FaleConosco/index.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyles/>
-    <Homepage />
+    <FaleConosco />
   </StrictMode>,
 )
---
+++ src/pages/FaleConosco/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container } from './styles';
+import { Header } from '../../components/Header';
+
+
+
+export function FaleConosco(){
+  return (
+    <Container>
+      <Header />
+      <h1>Fale Conosco</h1>
+      <p>Entre em contato conosco através do formulário abaixo:</p>
+      <form>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Mensagem:</label>
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </Container>
+  );
+};
+
+
